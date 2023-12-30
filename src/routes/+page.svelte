989011d851bd36f46041d2d13ce8c57ed77c651d
James Vaughan <contact@jamesvaughan.ca>
2023-12-30 18:20:35 +0000

    Start populating homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,83 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    function githubLink(project:String, displayName: String) {
+        return '<a href="https://github.com/J-Vaughan/' + project + '">'
+            + displayName + ' (' + project + ')</a>';
+    }
+</script>
+
+<!-- news ticker line -->
+
+
+<h1>James Vaughan</h1>
+<h2>Space Systems Design Student</h2>
+<p>Carleton University</p>
+<p>Ottawa, Ontario</p>
+<p>Canada, Earth</p>
+
+<h1>Interests</h1>
+<ul>
+    <li>
+        <h2>Control Systems</h2>
+        Want to make self-hovering rocket, in that it launches from a normal
+        pad, is powered by a single (possibly two) model rocket engines, and
+        is capable of keeping itself upright and at the prescribed altitude.
+    </li>
+    <li>
+        <h2>Mission Design</h2>
+    </li>
+    <li>
+        <h2>Language Design</h2>
+        Frazier, physical computing language with units, bases, and symbolic 
+        computing.
+    </li>
+    <li>
+        <h2>Design</h2>
+        <h3>Physical</h3>
+            <ul>
+                <li>
+                    Split (Chording?) keyboard
+                </li>
+                <li>
+                    Augmented reality applications
+                </li>
+                <li>
+                    Commie blocs
+                </li>
+            </ul>
+        <h3>Digital</h3>
+            <ul>
+                <li>
+                    Digital identity (email address replacement)
+                </li>
+                <li>
+                    Data archival and preservation
+                </li>
+            </ul>
+    </li>
+    <li>
+        <h2>Plain-text Computing</h2>
+        Accounting via ledger, as well as investigation into plaintext formats
+        for traditionally binary file formats, like CAD files or rich documents.
+    </li>
+    <li>
+        <h2>Decentralization</h2>
+        Dislike the internet.
+    </li>
+    <li>
+        <h2>Future Technologies</h2>
+        Human-computer interfaces, implants, and replacements.
+    </li>
+</ul>
+
+<h1>Certifications</h1>
+<ul>
+    <li>Ansys STK 12 1</li>
+</ul>
+
+<h1>Projects</h1>
+<ul>
+    <li>
+        <!--{githubLink("jamesvaughan.ca", "Personal Website")}-->
+        
+    </li>
+</ul>
